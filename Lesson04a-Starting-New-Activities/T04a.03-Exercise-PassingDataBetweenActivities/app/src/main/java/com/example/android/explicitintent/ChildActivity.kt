0fc6f55acc210fea35cb4d8dbefda24386777d9d
Siena Aguayo <siena.aguayo@gmail.com>
2018-01-21 16:33:17 -0800

    Kotlin Lesson 5.10 Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities convert to Kotlin

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.kt
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.kt
@@ -13,31 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.explicitintent;
+package com.example.android.explicitintent
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
-public class ChildActivity extends AppCompatActivity {
+class ChildActivity : AppCompatActivity() {
 
     /* Field to store our TextView */
-    private TextView mDisplayText;
+    private var mDisplayText: TextView? = null
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_child);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_child)
 
         /* Typical usage of findViewById... */
-        mDisplayText = (TextView) findViewById(R.id.tv_display);
+        mDisplayText = findViewById(R.id.tv_display) as TextView
 
         // TODO (3) Use the getIntent method to store the Intent that started this Activity in a variable
 
         // TODO (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
 
-            // TODO (5) If the Intent contains the correct extra, retrieve the text
+        // TODO (5) If the Intent contains the correct extra, retrieve the text
 
-            // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
+        // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
     }
 }
---
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.explicitintent
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    /* Fields that will store our EditText and Button */
+    private var mNameEntry: EditText? = null
+    private var mDoSomethingCoolButton: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+         * Using findViewById, we get a reference to our Button from xml. This allows us to
+         * do things like set the onClickListener which determines what happens when the button
+         * is clicked.
+         */
+        mDoSomethingCoolButton = findViewById(R.id.b_do_something_cool) as Button
+        mNameEntry = findViewById(R.id.et_text_entry) as EditText
+
+        /* Setting an OnClickListener allows us to do something when this button is clicked. */
+        mDoSomethingCoolButton!!.setOnClickListener {
+            // TODO (1) Retrieve the text from the EditText and store it in a variable
+            val context = this@MainActivity
+
+            /* This is the class that we want to start (and open) when the button is clicked. */
+            val destinationActivity = ChildActivity::class.java
+
+            val startChildActivityIntent = Intent(context, destinationActivity)
+
+            // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
+            startActivity(startChildActivityIntent)
+        }
+    }
+}
+
