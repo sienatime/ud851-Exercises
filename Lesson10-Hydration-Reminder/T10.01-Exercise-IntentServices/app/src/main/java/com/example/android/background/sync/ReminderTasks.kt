4195f28728a31f06485da6cbcb6931691e184deb
Siena Aguayo <siena.aguayo@gmail.com>
2018-03-10 17:36:14 -0800

    L 10.1 convert/enable Kotlin

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.kt
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.kt
@@ -1,4 +1,4 @@
-package com.example.android.background.sync;
+package com.example.android.background.sync
 
 // TODO (1) Create a class called ReminderTasks
 
@@ -12,3 +12,7 @@ package com.example.android.background.sync;
 // TODO (3) Create a private static void method called incrementWaterCount
 // TODO (4) Add a Context called context to the argument list
 // TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+
+internal class ReminderTasks {
+
+}
