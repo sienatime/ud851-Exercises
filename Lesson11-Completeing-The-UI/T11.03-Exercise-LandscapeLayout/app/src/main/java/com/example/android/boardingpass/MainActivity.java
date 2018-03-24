368a8b89147cedbac9c3a39095b5f8f9d0187a81
Siena Aguayo <siena.aguayo@gmail.com>
2018-03-24 15:52:05 -0700

    L 11.3

--- Lesson11-Completeing-The-UI/T11.03-Exercise-LandscapeLayout/app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ Lesson11-Completeing-The-UI/T11.03-Exercise-LandscapeLayout/app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -48,19 +48,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayBoardingPassInfo(BoardingPassInfo info) {
-
         mBinding.textViewPassengerName.setText(info.passengerName);
-        // TODO (7) Use the flightInfor attribute in mBinding below to get the appropiate text Views
-        mBinding.textViewOriginAirport.setText(info.originCode);
-        mBinding.textViewFlightCode.setText(info.flightCode);
-        mBinding.textViewDestinationAirport.setText(info.destCode);
+        mBinding.flightInfo.textViewOriginAirport.setText(info.originCode);
+        mBinding.flightInfo.textViewFlightCode.setText(info.flightCode);
+        mBinding.flightInfo.textViewDestinationAirport.setText(info.destCode);
 
         SimpleDateFormat formatter = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault());
         String boardingTime = formatter.format(info.boardingTime);
         String departureTime = formatter.format(info.departureTime);
         String arrivalTime = formatter.format(info.arrivalTime);
 
-        mBinding.textViewBoardingTime.setText(boardingTime);
+        mBinding.flightInfo.textViewBoardingTime.setText(boardingTime);
         mBinding.textViewDepartureTime.setText(departureTime);
         mBinding.textViewArrivalTime.setText(arrivalTime);
 
@@ -73,11 +71,10 @@ public class MainActivity extends AppCompatActivity {
                 hoursUntilBoarding,
                 minutesLessHoursUntilBoarding);
 
-        mBinding.textViewBoardingInCountdown.setText(hoursAndMinutesUntilBoarding);
-        // TODO (8) Use the boardingInfo attribute in mBinding below to get the appropiate text Views
-        mBinding.textViewTerminal.setText(info.departureTerminal);
-        mBinding.textViewGate.setText(info.departureGate);
-        mBinding.textViewSeat.setText(info.seatNumber);
+        mBinding.flightInfo.textViewBoardingInCountdown.setText(hoursAndMinutesUntilBoarding);
+        mBinding.boardingInfo.textViewTerminal.setText(info.departureTerminal);
+        mBinding.boardingInfo.textViewGate.setText(info.departureGate);
+        mBinding.boardingInfo.textViewSeat.setText(info.seatNumber);
     }
 }
 
