5619f725e6aaf7721296faa5eb41d33ad4d282e5
Siena Aguayo <siena.aguayo@gmail.com>
2018-06-16 14:50:02 -0700

    L 9b.3

--- Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -25,6 +25,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
+import com.example.android.todolist.database.AppDatabase;
 
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     private RecyclerView mRecyclerView;
     private TaskAdapter mAdapter;
 
-    // TODO (1) Create AppDatabase member variable for the Database
+    private AppDatabase appDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,7 +93,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
             }
         });
 
-        // TODO (2) Initialize member variable for the data base
+        appDb = AppDatabase.getInstance(getApplicationContext());
     }
 
     /**
@@ -103,8 +104,9 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     @Override
     protected void onResume() {
         super.onResume();
-        // TODO (3) Call the adapter's setTasks method using the result
-        // of the loadAllTasks method from the taskDao
+        // it's better to do this onResume instead of onCreate because if it were done in
+        // onCreate, we would only get a refreshed task list when the activity was destroyed
+        mAdapter.setTasks(appDb.taskDao().loadAllTasks());
     }
 
     @Override
